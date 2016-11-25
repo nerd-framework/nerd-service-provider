a70b5a4a3fad4a8afc04ff02027d5fd678cd3864
Roman Gemini <roman@homefs.biz>
2016-11-25 16:58:08 +0200

    Update ServiceProvider.

--- src/Nerd/Framework/ServiceProvider.php
+++ src/Nerd/Framework/ServiceProvider.php
@@ -6,18 +6,13 @@ use Nerd\Framework\Services\ServiceProviderContract;
 
 abstract class ServiceProvider implements ServiceProviderContract
 {
-    private $app;
+    /**
+     * @var ApplicationContract
+     */
+    protected $app;
 
     public function __construct(ApplicationContract $app)
     {
         $this->app = $app;
-    }
-
-    /**
-     * @return ApplicationContract
-     */
-    protected function getApp()
-    {
-        return $this->app;
     }
 }
